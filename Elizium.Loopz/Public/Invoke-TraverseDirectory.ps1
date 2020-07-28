39d06f48783d1082a51d6eca7491359499a91cce
plastikfan <plastikfan@outlook.com>
2020-07-28 19:46:11 +0100

    (feat #4) THIS IS A FUCKED UP CHECKIN (see comments in this issue)

--- Elizium.Loopz/Public/Invoke-TraverseDirectory.ps1
+++ Elizium.Loopz/Public/Invoke-TraverseDirectory.ps1
@@ -3,37 +3,103 @@ function Invoke-TraverseDirectory {
   [CmdletBinding()]
   param
   (
-    [parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
+    [ValidateScript( { Test-path -Path $_; })]
     [String]$SourcePath,
   
-    [parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
     [String]$DestinationPath,
 
-    [parameter(Mandatory = $true)]
-    [String]$Suffix,
+    [Parameter()]
+    [String[]]$Suffixes = @('*'),
 
-    [parameter(Mandatory = $true)]
-    [scriptblock]$SourceFileBlock,
+    [Parameter()]
+    [String[]]$DirectoryIncludes = @('*'),
 
-    [parameter(Mandatory = $true)]
+    [Parameter(Mandatory)]
     [System.Collections.Hashtable]$PassThru,
 
-    [parameter()]
+    [Parameter(Mandatory)]
+    [scriptblock]$SourceFileBlock,
+
+    [Parameter()]
     [scriptblock]$SourceDirectoryBlock = ( { return $true; })
   )
 
-  # Deal with top level files
-  #
-
-  {
-    Invoke-ForeachFile -Directory $sourcePath -inclusions $inclusions -body $onSourceFile -propertyBag $PassThru `
-      -summary $summary -Verb;
-
-    [string]$directoryPath = './Tests/Data/fefsi';
-    Get-ChildItem $directoryPath | Invoke-ForeachFsItem -Block $block -File;
+  Write-Host "Invoke-TraverseDirectory >>> SourcePath: '$SourcePath'"
+  [string[]]$inclusions = @();
+  $Suffixes | ForEach-Object {
+    $inclusions += $_.StartsWith('*.') ? $_ : ('*.' + $_)
   }
 
+  $sourcePathWithWildCard = $SourcePath.Contains('*') ? $SourcePath : (Join-Path -Path $SourcePath -ChildPath '*');
+
+  Write-Host "Invoke-TraverseDirectory >>> sourcePathWithWildCard: '$sourcePathWithWildCard'"
+
+  # Invoke the source directory block for this directory
+  #
+  # $SourceDirectoryBlock.Invoke($sourcePath, $PassThru);
+
+  # Finally traverse this directory's child directories
+  #
+  [System.IO.DirectoryInfo[]]$sourceDirectoryInfos = Get-ChildItem -Path $sourcePathWithWildCard `
+    -Directory -Include $DirectoryIncludes;
+  $sourceDirectoryInfos | Invoke-ForeachFsItem -Directory -Block $SourceDirectoryBlock -PassThru $PassThru;
   
+  # Deal with top level files for this directory
+  # WARNING: you can't use wild card with LiteralPath
+  #
+  [System.IO.FileInfo[]]$sourceFileInfos = Get-ChildItem -Path $sourcePathWithWildCard `
+    -File -Include $inclusions;
+  $sourceFileInfos | Invoke-ForeachFsItem -File -Block $SourceFileBlock -PassThru $PassThru;
+  
+  # Convert directory contents
+  #
+  [scriptblock]$doTraversal = { param(
+      $_underscore,
+      $_index,
+      $_passThru,
+      $_trigger
+    )
+
+    # Argh, this looks like mirror directory not traverse. Big blunder here.
+    #
+
+    # BUG: Make sure that ROOT-SOURCE ends with /* so that include/exclude works
+    # Why the F is rootSource an array of objects instead of a string?
+    $rootSource = $_passThru['ROOT-SOURCE'];
+    $rootDestination = $_passThru['ROOT-DESTINATION'];
+
+    $sourceDirectoryFullName = $_underscore.FullName;
+    $contentsColour = "Green";
+
+    # sourceDirectoryFullName must end with directory separator
+    #
+    if (-not($sourceDirectoryFullName.EndsWith([System.IO.Path]::DirectorySeparatorChar))) {
+      $sourceDirectoryFullName += [System.IO.Path]::DirectorySeparatorChar;
+    }
+
+    $destinationBranch = remove-SingleSubString -Target $sourceDirectoryFullName -Subtract $rootSource;
+    $destinationDirectory = Join-Path -Path $rootDestination  -ChildPath $destinationBranch;
+    # Write-Host "@@@ sourceDirectoryFullName: $sourceDirectoryFullName"
+    # Write-Host "--- Subtracting ROOT-SOURCE: '$rootSource' from '$sourceDirectoryFullName'";
+    # Write-Host "+++ destination directory: $destinationDirectory"
+    # Write-PairInColour @( ("destination directory", "Yellow"), ($destinationDirectory, "Red") );
+
+    Invoke-TraverseDirectory -SourcePath $sourceDirectoryFullName -DestinationPath $destinationDirectory `
+      -Suffixes $inclusions -DirectoryIncludes $DirectoryIncludes -PassThru $_passThru `
+      -SourceFileBlock $SourceFileBlock -SourceDirectoryBlock $SourceDirectoryBlock;
+
+    return @{ Message = "*** Convert directory contents";
+      Product = $_underscore; Colour = $contentsColour 
+    };
+
+    # Finally traverse this directory's child directories
+    #
+    # [System.IO.DirectoryInfo[]]$sourceDirectoryInfos = Get-ChildItem -Path $sourcePathWithWildCard `
+    #   -Directory -Include $DirectoryIncludes;
+    $sourceDirectoryInfos | Invoke-ForeachFsItem -Directory -Block $doTraversal -PassThru $PassThru;
+  } # doTraversal
 }
 
 function Invoke-TraverseDirectory_Legacy {
@@ -74,19 +140,19 @@ function Invoke-TraverseDirectory_Legacy {
   [CmdletBinding()]
   param
   (
-    [parameter(Mandatory = $true)]
+    [parameter(Mandatory)]
     [String]$sourcePath,
   
-    [parameter(Mandatory = $true)]
+    [parameter(Mandatory)]
     [String]$destinationPath,
 
-    [parameter(Mandatory = $true)]
+    [parameter(Mandatory)]
     [String]$suffix,
 
-    [parameter(Mandatory = $true)]
+    [parameter(Mandatory)]
     [scriptblock]$onSourceFile,
 
-    [parameter(Mandatory = $true)]
+    [parameter(Mandatory)]
     [System.Collections.Hashtable]$PassThru,
 
     [parameter()]
---
+++ Elizium.Loopz/Tests/Public/Invoke-TraverseDirectory.tests.ps1
@@ -0,0 +1,66 @@
+
+Describe 'Invoke-TraverseDirectory' -Tag 'Current' {
+  BeforeAll {
+    Get-Module Elizium.Loopz | Remove-Module
+    Import-Module .\Output\Elizium.Loopz\Elizium.Loopz.psm1 `
+      -ErrorAction 'stop' -DisableNameChecking
+  }
+
+  Context 'given: directory tree' {
+    It 'Should: traverse' {
+      $sourcePath = '.\Tests\Data\traverse\';
+      $destinationPath = '~\dev\TEST\';
+
+      # NEED A TRAILING /
+      [string]$resolvedSourcePath = Convert-Path '.\Tests\Data\traverse';
+      [string]$destinationPath = Convert-Path '~\dev\TEST';
+
+      [System.Collections.Hashtable]$passThru = @{
+        # Do NOT use Resolve-Path with a wild-card
+        #
+        'ROOT-SOURCE'      = $resolvedSourcePath
+        'ROOT-DESTINATION' = $destinationPath;
+      }
+
+      [scriptblock]$feSourceFileblock = {
+        param(
+          [Parameter(Mandatory)]
+          $_underscore,
+
+          [Parameter(Mandatory)]
+          [int]$_index,
+
+          [Parameter(Mandatory)]
+          [System.Collections.Hashtable]$_passThru,
+
+          [Parameter(Mandatory)]
+          [boolean]$_trigger
+        )
+        Write-Host "    [+] File: '$($_underscore.Name)'"
+        @{ Product = $_underscore }
+      }
+
+      [scriptblock]$feDirectoryFileBlock = {
+        param(
+          [Parameter(Mandatory)]
+          $_underscore,
+
+          [Parameter(Mandatory)]
+          [int]$_index,
+
+          [Parameter(Mandatory)]
+          [System.Collections.Hashtable]$_passThru,
+
+          [Parameter(Mandatory)]
+          [boolean]$_trigger
+        )
+        Write-Host "  [*] Directory: '$($_underscore)', type: $($_underscore.GetType())"
+        @{ Product = $_underscore }
+      }
+
+      Invoke-TraverseDirectory -SourcePath $sourcePath -DestinationPath $destinationPath `
+        -PassThru $passThru `
+        -SourceFileBlock $feSourceFileblock -SourceDirectoryBlock $feDirectoryFileBlock;
+    }
+  }
+}
