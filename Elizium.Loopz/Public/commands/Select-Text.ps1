2bb323a5cfd2bd2b4204cb991f9c6c71c928b1f3
plastikfan <plastikfan@outlook.com>
2020-12-31 16:06:59 +0000

    (feat #80): Add greps/Select-Text command

---
+++ Elizium.Loopz/Public/commands/Select-Text.ps1
@@ -0,0 +1,82 @@
+﻿
+function Select-Text {
+  [CmdletBinding()]
+  [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingInvokeExpression', '')]
+  [Alias("greps")]
+  param
+  (
+    [parameter(Mandatory = $true, Position = 0)]
+    [ValidateNotNullOrEmpty()]
+    [String[]] $patterns,
+  
+    [parameter(Position = 1)]
+    [ValidateNotNullOrEmpty()]
+    [String]$filter = $(Get-EnvironmentVariable 'GREPS_FILTER')
+  )
+
+  function build-command {
+    [OutputType([string])]
+    param(
+      [String]$Pattern,
+      [String]$Filter,
+      [Switch]$Pipe,
+      [string]$NotOpSymbol = '!'
+    )
+    [string]$platform = Get-PlatformName;
+    [string]$builder = [string]::Empty;
+
+    if ($Pipe.ToBool()) {
+      $builder += ' | ';
+    }
+
+    if ($platform -eq 'windows') {
+      $builder += 'select-string ';
+      if ($pattern.StartsWith($NotOpSymbol)) {
+        $builder += ('-notmatch -pattern "{0}" ' -f $Pattern.Substring(1));
+      }
+      else {
+        $builder += ('-pattern "{0}" ' -f $Pattern);
+      }
+    }
+    else {
+      $builder += 'grep ';
+      if ($pattern.StartsWith($NotOpSymbol)) {
+        $builder += ('-v -i "{0}" ' -f $Pattern.Substring(1));
+      }
+      else {
+        $builder += ('-i "{0}" ' -f $Pattern);
+      }
+    }
+
+    if (-not([string]::IsNullOrWhiteSpace($Filter))) {
+      $builder += "$Filter ";
+    }
+
+    return $builder;
+  } # build-command
+
+  [string]$command = [string]::Empty;
+  [int]$count = 0;
+
+  foreach ($pattern in $patterns) {
+    $count++;
+
+    if ($count -eq 1) {
+      $command = build-command -Pattern $patterns[0] -Filter $filter;
+    }
+    else {
+      $segment = build-command -Pipe -Pattern $pattern;
+      $command += $segment;
+    }
+  }
+
+  [hashtable]$signals = $(Get-Signals);
+  [hashtable]$theme = $(Get-KrayolaTheme);
+  [writer]$writer = New-Writer -Theme $theme;
+  [couplet]$formattedSignal = Get-FormattedSignal -Name 'GREPS' -Value $command -Signals $signals;
+  Invoke-Expression $command;
+
+  $null = $writer.blue().Text($formattedSignal.Key). `
+    red().Text(' --> '). `
+    green().Text($formattedSignal.Value);
+}
