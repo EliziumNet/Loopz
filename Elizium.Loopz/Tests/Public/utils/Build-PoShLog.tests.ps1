9b2635c1735fb4129698be08cbe544b08b2adbdf
plastikfan <plastikfan@outlook.com>
2021-06-04 15:59:32 +0100

    style(poshlog): change by renaming ChangeLog to PoShLog (#154)
    style(poshlog): change by renaming ChangeLog to PoshLog (#154)
    
    style(poshlog): change by renaming Build-ChangeLog.ps1 to Build-PoShLog.ps1 (#154)
    
    style(poshlog): change by renaming command Build-ChangeLog to Build-PoShLog (#154)
    
    style(poshlog): change by renaming Build-ChangeLog.tests.ps1 to Build-PoShLog.tests.ps1 (#154)
    
    style(poshlog): update docs to reflect Build-ChangeLog being renamed to Build-PoShLog (#154)
    
    style(poshlog): update docs, rename resources/docs/build-changelog.md to changelog.md (#154)
    
    style(poshlog): change by renaming docs/changelog.md to posh-log.md (#154)

--- Elizium.Loopz/Tests/Public/utils/Build-PoShLog.tests.ps1
+++ Elizium.Loopz/Tests/Public/utils/Build-PoShLog.tests.ps1
@@ -1,13 +1,13 @@
 using module Elizium.Klassy;
 
-Describe 'Build-ChangeLog' -Tag 'chog' {
+Describe 'Build-PoShLog' -Tag 'plog' {
   BeforeAll {
     Get-Module Elizium.Loopz | Remove-Module
     Import-Module .\Output\Elizium.Loopz\Elizium.Loopz.psm1 `
       -ErrorAction 'stop' -DisableNameChecking
 
     [string]$script:ROOT = 'root';
-    [string]$script:DIRECTORY = [ChangeLogSchema]::DIRECTORY;
+    [string]$script:DIRECTORY = [PoShLogProfile]::DIRECTORY;
     [string]$script:_rootPath = Join-Path -Path $TestDrive -ChildPath $ROOT;
   }
 
@@ -16,13 +16,13 @@ Describe 'Build-ChangeLog' -Tag 'chog' {
       Context 'and: requested options do not exist' {
         It 'should: create new options config' {
           [PSCustomObject]$optionsInfo = [PSCustomObject]@{
-            Base          = '-changelog.options';
-            DirectoryName = [ChangeLogSchema]::DIRECTORY;
+            Base          = '-poshlog.options';
+            DirectoryName = [PoShLogProfile]::DIRECTORY;
             GroupBy       = 'scope/type/change/break';
             Root          = $_rootPath;
           }
 
-          [ChangeLogOptionsManager]$manager = New-ChangeLogOptionsManager -OptionsInfo $optionsInfo;
+          [PoShLogOptionsManager]$manager = New-PoShLogOptionsManager -OptionsInfo $optionsInfo;
           [boolean]$withEmoji = $true;
 
           [PSCustomObject]$options = $manager.FindOptions('Elizium', $withEmoji);
@@ -37,24 +37,24 @@ Describe 'Build-ChangeLog' -Tag 'chog' {
       Context 'and: options file exists' {
         It 'should: build change log' {
           [PSCustomObject]$optionsInfo = [PSCustomObject]@{
-            Base          = '-changelog.options';
+            Base          = '-poshlog.options';
             DirectoryName = $DIRECTORY;
             GroupBy       = 'scope/type/change/break';
             Root          = $_rootPath;
           }
           [string]$directoryPath = Join-Path -Path $_rootPath -ChildPath $DIRECTORY;
-          [string]$optionsFileName = 'Test-emoji-changelog.options.json';
+          [string]$optionsFileName = 'Test-emoji-poshlog.options.json';
           [string]$testPath = "./Tests/Data/changelog/$optionsFileName";
 
           [void]$(New-Item -ItemType 'Directory' -Path $directoryPath);
           [string]$destinationPath = Join-Path -Path $directoryPath -ChildPath $optionsFileName;
           Copy-Item -LiteralPath $testPath -Destination $destinationPath;
 
-          [ChangeLogOptionsManager]$manager = New-ChangeLogOptionsManager -OptionsInfo $optionsInfo;
+          [PoShLogOptionsManager]$manager = New-PoShLogOptionsManager -OptionsInfo $optionsInfo;
           [boolean]$withEmoji = $true;
 
           [PSCustomObject]$options = $manager.FindOptions('Test', $withEmoji);
-          [ChangeLog]$changeLog = New-ChangeLog -Options $options;
+          [PoShLog]$changeLog = New-PoShLog -Options $options;
           [string]$outputPath = Join-Path -Path $directoryPath -ChildPath 'ChangeLog.test-emojis.md';
           [string]$content = $changeLog.Build();
           $changeLog.Save($content, $outputPath);
@@ -65,12 +65,12 @@ Describe 'Build-ChangeLog' -Tag 'chog' {
     }
   }
 
-  Describe 'Build-ChangeLog' {
+  Describe 'Build-PoShLog' {
     BeforeEach {
       $env:PesterTestDrive = $TestDrive;
-      [string]$directoryPath = Join-Path -Path $TestDrive -ChildPath $([ChangeLogSchema]::DIRECTORY);
+      [string]$directoryPath = Join-Path -Path $TestDrive -ChildPath $([PoShLogProfile]::DIRECTORY);
       [string]$script:_optionsFileName = $(
-        Join-Path -Path $directoryPath -ChildPath 'Elizium-emoji-changelog.options.json'
+        Join-Path -Path $directoryPath -ChildPath 'Elizium-emoji-poshlog.options.json'
       );
 
       [string]$script:_markdownFileName = $(
@@ -81,10 +81,10 @@ Describe 'Build-ChangeLog' -Tag 'chog' {
     Context 'given: Eject Emoji options' {
       Context 'and: requested options do not exist' {
         It 'should: create new options config' {
-          Build-ChangeLog -Name 'Elizium' -Emoji -Eject -Test;
+          Build-PoShLog -Name 'Elizium' -Emoji -Eject -Test;
           Test-Path -Path $_optionsFileName | Should -BeTrue;
 
-          Build-ChangeLog -Name 'Elizium' -Emoji -Test;
+          Build-PoShLog -Name 'Elizium' -Emoji -Test;
           Test-Path -Path $_markdownFileName | Should -BeTrue;
         }
       }
@@ -93,7 +93,7 @@ Describe 'Build-ChangeLog' -Tag 'chog' {
     Context 'given: Create Emoji ChangeLog' {
       Context 'and: requested options not exist' {
         It 'should: just eject the required config' -Tag 'Current' {
-          Build-ChangeLog -Name 'Elizium' -Emoji -Test;
+          Build-PoShLog -Name 'Elizium' -Emoji -Test;
           Test-Path -Path $_optionsFileName | Should -BeTrue;
         }
       }
